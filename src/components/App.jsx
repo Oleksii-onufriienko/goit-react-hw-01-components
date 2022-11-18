3b4387bb20e3a3a392a392638ab7d1cfff7433bc
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-11-18 10:24:44 +0200

    1 task without style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import Profile from '../components/Profile/Profile'
+import user from '../components/user.json'
+
+console.log(user);
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+      <Profile {...user} />
+</div>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
