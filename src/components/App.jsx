8f72b3f1c567f1522b06b26bdf1d652484f2f17f
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-11-18 19:21:39 +0200

    task 1,2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,16 @@
-import Profile from '../components/Profile/Profile'
-import user from '../components/user.json'
+import Profile from 'components/Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import user from 'components/user.json';
+import data from 'components/data.json';
 
-console.log(user);
+// console.log(user);
 
 export const App = () => {
   return (
     <div>
       <Profile {...user} />
-</div>
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    // >
-    //   React homework template
-    // </div>
+      <Statistics filesType={data} />
+    </div>
+
   );
 };
