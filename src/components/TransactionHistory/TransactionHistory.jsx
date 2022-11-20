5783e572bf18c15db6abae24855b921a0dd6bef4
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-11-20 08:51:06 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function TransactionHistory({items}) {
     return (
-      <table className={css.table__center}>
+<table className={css.table__center}>
   <thead>
     <tr>
       <th>Type</th>
